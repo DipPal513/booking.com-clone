6c46750b41671efc9209625b87dd59123da2ccfc
dippal513 <dip.pal.513@gmail.com>
2023-12-23 12:00:01 +0600

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const Header = () => {
+    const [mobileMenuOpen,setMobileMenuOpen] = useState(false)
+  return (
+    <header className="bg-[#13894]">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <Link href={"/"}>
+            <h1 className="text-xl font-bold">Booking.com</h1>
+          </Link>
+        </div>
+        <span className="sr-only">Open mobile menu</span>
+        <Bars3Icon className="h-6 w-6" area-hidden="true" />
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
